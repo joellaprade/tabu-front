ff5a3e4bf40c554f95a741a7a93ea1bb669cb80c
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-02-01 14:04:39 -0600

    init front

---
+++ components/head.js
@@ -0,0 +1,7 @@
+document.querySelector('head').innerHTML += `<link rel="stylesheet" href="/global/global.css">
+                                            <link rel="shortcut icon" type="image/png" href="/assets/favicon.png">
+                                            <link rel="preconnect" href="https://fonts.googleapis.com">
+                                            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                                            <link href="" rel="stylesheet">
+                                            <meta charset="UTF-8">
+                                            <meta name="viewport" content="width=device-width, initial-scale=1.0">`
